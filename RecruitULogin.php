1484f0e81f15379072ab626ff00984db3c73c105
Saba <95149858+sabawoonpopal@users.noreply.github.com>
2024-02-12 15:43:23 -0500

    Update RecruitULogin.php

--- RecruitULogin.php
+++ RecruitULogin.php
@@ -6,6 +6,11 @@
     <title>RecruitU Login</title>
     <link rel="stylesheet" href="loginStyle.css">
 </head>
+<header>
+	<img src="RecruitULogo.png" alt="RecruitU Logo" class="logo" width="100">
+        <h1>Welcome back to RecruitU!</h1>
+        <p>Ready to continue your search for athletic success?</p>
+    </header>
 <body>
     <div class="login-container">
         <h2>Login to RecruitU</h2>
@@ -15,11 +20,11 @@
 
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
-
+		<br>
             <button type="submit">Login</button>
         </form>
         
-        <p>Don't have an account? <a href="RecruitURegister.html">Register here</a>.</p>
+        <p>Don't have an account? <a href="RecruitURegister.php">Register here</a>.</p>
     </div>
 </body>
 </html>
