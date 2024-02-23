2482fe24bf73521a35d8d39a3b817b8e675a4cf9
KyleSinclair89 <98722268+KyleSinclair89@users.noreply.github.com>
2024-02-23 13:50:52 -0500

    Update RecruitULogin.php

--- RecruitULogin.php
+++ RecruitULogin.php
@@ -77,7 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 <body>
     <div class="login-container">
         <h2>Login to RecruitU</h2>
-        <form action="RecruitUProfilePage.php" method="post">
+        <form action="RecruitULogin.php" method="post">
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
 
