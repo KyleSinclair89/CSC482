0c38e043eb762582f33084b156e6b4205280de40
KyleSinclair89 <98722268+KyleSinclair89@users.noreply.github.com>
2024-04-02 14:08:56 -0400

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,29 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  // Get the data from the form
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  // Limit the message sent to 150 words
+  $message = substr($message, 0, 150);
+
+  // Settings for the email
+  $to = "kylesinclair@mail.adelphi.edu";
+  $headers = "From: $name <$email>";
+  $body = "Subject: $subject \n\n$message";
+
+  // Send the email
+  if(mail($to,$subject,$body,$headers)){
+    echo "Email sent successfully! Returning home..."
+    header("location: RecruitUHomePage.php");
+					exit;
+  }
+  else{
+    echo "Failed to send email! Please try again."
+      header("location: RecruitUContactUs.html");
+					exit;
+  }
+}
+?>
