1e06d7e52d17921d6a80734f34ed395e4a38bcc3
KyleSinclair89 <98722268+KyleSinclair89@users.noreply.github.com>
2024-04-15 15:40:41 -0400

    Update RecruitUHomePage.php

--- RecruitUHomePage.php
+++ RecruitUHomePage.php
@@ -157,6 +157,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <input type="file" id="file-upload" name="file" style="display: none;">
 <input type="submit" id="submitButton" style="font-weight: bold; background-color: #af0a06; color: white; padding: 10px; text-decoration: none; border-radius: 5px; border: 2.5px solid black; box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); transition: box-shadow 0.3s, background-color 0.3s, color 0.3s; top: 0; right: 0; z-index: 1; margin-top: 50px; cursor: pointer;" onmouseover="this.style.backgroundColor='#000'; this.style.color='#EE2737';" onmouseout="this.style.backgroundColor='#af0a06'; this.style.color='white';"></label>
 </form>
+
+
+<br>
+<br>
+
+<form id="searchForm" action="searchresults.php" method="GET" style="display: inline-block;"> 
+	<input type="text" id="lastName" name="lastName" placeholder="Last Name" style="margin-left: 10px;"> 
+	<button type="submit" style="background-color: #af0a06; color: white; padding: 10px; text-decoration: none; border-radius: 5px; border: 2.5px solid black; box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); transition: box-shadow 0.3s, background-color 0.3s, color 0.3s; margin-left: 10px;">Search</button> 
+</form>
+
 <br>
 <?php
     // Call the function to display all posts
